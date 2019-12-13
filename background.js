e7dd0e575a0bec975d07438d1087126084f78dee
Brent Hughes <brentahughes@gmail.com>
2019-12-13 15:13:39 -0600

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,4 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-    console.log("clicked yay");
-    console.log(tab);
     chrome.windows.create({
         "url": tab.url,
         "type": "popup",
